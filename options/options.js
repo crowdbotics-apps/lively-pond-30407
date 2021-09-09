9e01928a9867b651f8e60a2ac30b88dc84abe1c7
Crowdbotics <team@crowdbotics.com>
2021-09-09 04:07:04 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "lively_pond_30407Identifier",
+  url: "https://lively_pond_30407Identifier.botics.co",
+  api: "https://lively_pond_30407Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
